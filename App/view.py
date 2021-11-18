b9ba03a10aea03c11f382d96a3be208d01bf308f
wthisalec <ja.charry@uniandes.edu.co>
2021-11-17 20:15:56 -0500

    "Laboratorio 9 - Entrega Final"

--- App/view.py
+++ App/view.py
@@ -31,6 +31,7 @@ import threading
 from App import controller
 from DISClib.ADT import stack
 assert config
+from time import process_time
 
 """
 La vista se encarga de la interacción con el usuario.
@@ -83,7 +84,13 @@ def optionThree(cont):
 
 
 def optionFour(cont, initialStation):
+    #PRUEBA DE EJECUCIÓN
+    start_time = process_time()
     controller.minimumCostPaths(cont, initialStation)
+    #PRUEBAS DE EJECUCIÓN
+    stop_time = process_time()
+    elapsed_time_mseg = (stop_time - start_time)*1000
+    print("tiempo de ejecución: " + str(elapsed_time_mseg))
 
 
 def optionFive(cont, destStation):
@@ -94,6 +101,8 @@ def optionFive(cont, destStation):
 
 
 def optionSix(cont, destStation):
+    #PRUEBA DE EJECUCIÓN
+    start_time = process_time()
     path = controller.minimumCostPath(cont, destStation)
     if path is not None:
         pathlen = stack.size(path)
@@ -103,6 +112,10 @@ def optionSix(cont, destStation):
             print(stop)
     else:
         print('No hay camino')
+    #PRUEBAS DE EJECUCIÓN
+    stop_time = process_time()
+    elapsed_time_mseg = (stop_time - start_time)*1000
+    print("tiempo de ejecución: " + str(elapsed_time_mseg))
 
 
 def optionSeven(cont):
